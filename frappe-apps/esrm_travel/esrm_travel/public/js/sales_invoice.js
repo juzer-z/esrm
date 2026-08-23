9a7e8b6a01ebf573280d5ac4b947f5d465f94dc8
juzer-z <195772158+juzer-z@users.noreply.github.com>
2026-08-23 12:51:39 +0600

    Restore administrator invoice cancellation action

--- frappe-apps/esrm_travel/esrm_travel/public/js/sales_invoice.js
+++ frappe-apps/esrm_travel/esrm_travel/public/js/sales_invoice.js
@@ -1,17 +1,30 @@
 frappe.ui.form.on("Sales Invoice", {
     refresh(frm) {
-        if (
-            frappe.session.user !== "Administrator"
-            || frm.is_new()
-            || frm.doc.docstatus !== 0
-        ) {
+        if (frappe.session.user !== "Administrator" || frm.is_new()) {
             return;
         }
 
-        frm.add_custom_button(
-            __("Submit Invoice"),
-            () => frm.savesubmit(),
-            __("Actions")
-        );
+        if (frm.doc.docstatus === 0) {
+            frm.add_custom_button(
+                __("Submit Invoice"),
+                () => frm.savesubmit(),
+                __("Actions")
+            );
+        }
+
+        if (frm.doc.docstatus === 1) {
+            frm.add_custom_button(
+                __("Cancel Invoice"),
+                () => {
+                    frappe.confirm(
+                        __("Cancel Sales Invoice {0}? This will reverse its accounting entries.", [
+                            frm.doc.name,
+                        ]),
+                        () => frm.savecancel()
+                    );
+                },
+                __("Actions")
+            );
+        }
     },
 });
